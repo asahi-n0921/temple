78deab6f13d2c3b7027ba7221891a46a1869f1a1
Nakasone <kokonoka@asahigh.com>
2020-05-24 13:38:51 +0900

    更新

--- js/style.js
+++ js/style.js
@@ -1,3 +1,4 @@
+// ハンバーガーメニュー
 $(function () {
   $('.menu').on('click', function () {
     $(this).toggleClass('active');
@@ -8,4 +9,30 @@ $(function () {
     $('#nav').toggleClass('active');
     $(".menu").toggleClass('active');
   })
+});
+
+// ハンバーガーメニュー（バックグラウンドの影付き）
+$(function () {
+	$('.ハンバーガーメニュー').click(function () {
+		$(this).toggleClass('active');
+		$('.バックグラウンド').fadeToggle();
+		$('.メニューの部分').toggleClass('open');
+	})
+	$('.バックグラウンド').click(function () {
+		$(this).fadeOut();
+		$('.ハンバーガーメニュー').removeClass('active');
+		$('.メニューの部分').removeClass('open');
+	});
+});
+
+// ヘッダースクロール
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
 });
